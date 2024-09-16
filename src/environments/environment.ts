ecfab54c5ddd3520081edf0b9b6fec05d6036494
jaman <krzysztofbalon123@gmail.com>
2024-09-16 20:13:45 +0200

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
-  production: false,
-
   SERVER_BASE_URL: 'https://localhost:1337',
 
-  SERVER_API_URL: 'http://qa-api-mock-3.eu-central-1.elasticbeanstalk.com',
+  SERVER_API_URL: 'https://localhost:1337/api',
 };
