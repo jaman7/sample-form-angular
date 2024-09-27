f43be3dd2ecc01e84834316768906247bf36692b
jaman7 <krzysztofbalon123@gmail.com>
2024-09-27 13:49:55 +0200

    init

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -77,7 +77,7 @@ export class CoreModule {
     parentModule: CoreModule
   ) {
     if (parentModule) {
-      throw new Error('CoreModule is already loaded. Import only in AppModule');
+      throw new Error('CoreModule is already loaded.');
     }
     ServiceLocator.injector = this.injector;
   }
